907feb2507e2510b6ceb637dfff76c5ce8e660ea
Leonel Sanagua <leonel.sanagua@gmail.com>
2023-11-07 16:38:17 -0300

    Se hizo validaciones del formulario

--- src/views/LoginScreen.jsx
+++ src/views/LoginScreen.jsx
@@ -3,10 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { login } from "../api/authApi";
 import "../css/login.css";
-import logo from "../assets/panadero.png"
-import user from "../assets/avatar24.png"
-import pass from "../assets/candado24.png"
-
+import logo from "../assets/panadero.png";
+import user from "../assets/avatar24.png";
+import pass from "../assets/candado24.png";
 
 const LoginScreen = () => {
   const navigate = useNavigate();
@@ -24,7 +23,7 @@ const LoginScreen = () => {
     setLoading(true);
     const respuesta = await login(data);
     console.log(respuesta);
-    alert(respuesta.msg)
+    alert(respuesta.msg);
     setLoginUser(respuesta);
     reset();
     setLoading(false);
@@ -34,41 +33,94 @@ const LoginScreen = () => {
       navigate("/");
     }
   };
-  
 
   return (
     <div className="container p-0 mt-5">
-		<div className="d-flex justify-content-center h-100 pt-5">
-			<div className="user_card">
-				<div className="d-flex justify-content-center">
-					<div className="brand_logo_container">
-						<img src={logo} className="brand_logo" alt="Logo"/>
-					</div>
-				</div>
-				<div className="d-flex justify-content-center form_container">
-					<form>
-						<div className="input-group mb-4">
-							<div className="input-group-append">
-								<span className="input-group-text"><img src={user} alt="Logo"/></span>
-							</div>
-							<input type="text" name="" className="form-control input_user" value="" placeholder="Email"/>
-						</div>
-						<div className="input-group mb-2">
-							<div className="input-group-append">
-								<span className="input-group-text"><img src={pass} alt="Logo"/></span>
-							</div>
-							<input type="password" name="" className="form-control input_pass" value="" placeholder="Contraseña"/>
-						</div>
-							<div className="d-flex justify-content-center mt-3 login_container mt-5">
-				 	<button type="button" name="button" className="btn login_btn ">Iniciar Sesión</button>
-				   </div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-  
-    
+      <div className="d-flex justify-content-center h-100 pt-5">
+        <div className="user_card">
+          <div className="d-flex justify-content-center">
+            <div className="brand_logo_container">
+              <img src={logo} className="brand_logo" alt="Logo" />
+            </div>
+          </div>
+          <div className="d-flex justify-content-center form_container">
+            <form noValidate onSubmit={handleSubmit(inicioSesion)}>
+              <div className="input-group mb-4">
+                <div className="input-group-append">
+                  <span className="input-group-text">
+                    <img src={user} alt="Logo" />
+                  </span>
+                </div>
+                <input
+                  type="email"
+                  id="Email-input"
+                  className="form-control"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: "Este campo es requerido",
+                  })}
+                  required
+                  disabled={loading ? true : false}
+                />
+                <p className="text-danger">{errors.email?.message}</p>
+              </div>
+              <div className="input-group mb-2">
+                <div className="input-group-append">
+                  <span className="input-group-text">
+                    <img src={pass} alt="Logo" />
+                  </span>
+                </div>
+                <input
+                  type="password"
+                  id="password-input"
+                  className="form-control"
+                  placeholder="Contraseña"
+                  {...register("password", {
+                    required: "Este campo es requerido",
+                    pattern: {
+                      value: /^.{8,16}$/i,
+                      message: "La Contraseña debe tener 8 caracteres mínimos",
+                    },
+                  })}
+                  required
+                  disabled={loading ? true : false}
+                />
+                <p className="text-danger">{errors.password?.message}</p>
+              </div>
+              <div className="d-flex justify-content-center mt-3 login_container mt-5">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={loading ? true : false}
+                >
+                  Iniciar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      {loginUser?.msg && (
+        <div className="row mt-3">
+          <div className="col">
+            <div className="alert alert-danger" role="alert">
+              {loginUser.msg}
+            </div>
+          </div>
+        </div>
+      )}
+      {loginUser?.errors && (
+        <div className="row mt-3">
+          {loginUser.errors.map((error, index) => (
+            <div className="col" key={index}>
+              <div className="alert alert-danger" role="alert">
+                {error.msg}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
