625deb8d44d61ce5b8151fd4c0e667fde00bda90
Leonardo Zamorano <nvreparaciones@gmail.com>
2023-11-11 17:24:16 -0300

    se edita panel de venta y stock

--- src/components/CardSaleApp.jsx
+++ src/components/CardSaleApp.jsx
@@ -1,50 +1,58 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
-import { AutoComplete, InputGroup } from "rsuite";
 import SearchIcon from "@rsuite/icons/Search";
+import { AutoComplete, InputGroup,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ButtonToolbar,
+  Notification,
+  Placeholder,
+} from "rsuite";
+
 
 function CardSaleApp() {
-  const styles = {
-    width: 300,
-    marginBottom: 10,
-  };
-
-  const initialData = [
-    "Tortillas Gruesas",
-    "Tortillas Finas",
-    "Pan",
-    "Pan Lactal",
-    "Azúcar",
-    "Leche",
-    "Huevos",
-    "Mantequilla",
-    "Harina",
-    "Levadura",
-    "Miel",
-    "Galletas",
-    "Croissants",
-    "Pastelitos",
-    "Donas",
-    "Bagels",
-    "Panes Integrales",
-    "Bollo",
-    "Panecillos",
-  ];
-
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [quantity, setQuantity] = useState(1);
 
   const handleAddToCart = (selectedProduct) => {
-    const productToAdd = {
-      name: selectedProduct,
-      price: 10, // Puedes ajustar esto según el precio real
-      quantity: quantity,
-    };
+    const productDetails = initialData.find(
+      (product) => product.name === selectedProduct
+    );
 
-    setSelectedProducts([...selectedProducts, productToAdd]);
-    setQuantity(1); // Restablecer la cantidad a 1 después de agregar al carrito
+    if (productDetails) {
+      const productToAdd = {
+        name: productDetails.name,
+        price: productDetails.price,
+        quantity: quantity,
+      };
+
+      setSelectedProducts([...selectedProducts, productToAdd]);
+      setQuantity(1);
+    }
   };
+  const handleRemoveFromCart = (index) => {
+    const updatedProducts = [...selectedProducts];
+    updatedProducts.splice(index, 1);
+    setSelectedProducts(updatedProducts);
+  };
+
+  const handleQuantityChange = (index, newQuantity) => {
+    // Clonar el array para no mutar el estado directamente
+    const updatedProducts = [...selectedProducts];
+    updatedProducts[index].quantity = newQuantity;
+
+    setSelectedProducts(updatedProducts);
+  };
+
+  const Message = React.forwardRef(({ type, ...rest }, ref) => {
+    return (
+      <Notification ref={ref} {...rest} type={type} header={type}>
+        <Placeholder.Paragraph style={{ width: 320 }} rows={3} />
+      </Notification>
+    );
+  });
 
   return (
     <div className="m-4">
@@ -55,9 +63,9 @@ function CardSaleApp() {
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             Busque el producto para agregar al carrito{" "}
-            <InputGroup inside style={styles}>
+            <InputGroup inside className="100-w mt-2 mb-2">
               <AutoComplete
-                data={initialData}
+                data={initialData.map((product) => product.name)}
                 onSelect={(value) => handleAddToCart(value)}
               />
               <InputGroup.Button tabIndex={-1}>
@@ -71,16 +79,71 @@ function CardSaleApp() {
                 <th>Cantidad</th>
                 <th>Producto</th>
                 <th>Precio</th>
+                <th>Acciones</th> {/* Nueva columna para acciones */}
               </tr>
             </thead>
             <tbody>
               {selectedProducts.map((product, index) => (
                 <tr key={index}>
-                  <td>{product.quantity}</td>
+                  <td>
+                    <Button
+                      className="btn-circular"
+                      size="md"
+                      color="green"
+                      appearance="subtle"
+                      onClick={() =>
+                        handleQuantityChange(index, product.quantity + 1)
+                      }
+                    >
+                      +
+                    </Button>{" "}
+                    {product.quantity}{" "}
+                    <Button
+                      className="btn-circular"
+                      circle
+                      size="md"
+                      color="red"
+                      appearance="subtle"
+                      onClick={() =>
+                        handleQuantityChange(
+                          index,
+                          Math.max(product.quantity - 1, 1)
+                        )
+                      }
+                    >
+                      -
+                    </Button>
+                  </td>
                   <td>{product.name}</td>
                   <td>${product.price * product.quantity}</td>
+                  <td>
+                    <Button
+                      className="btn-circular"
+                      circle
+                      size="md"
+                      color="red"
+                      appearance="subtle"
+                      onClick={() => handleRemoveFromCart(index)}
+                    >
+                      X
+                    </Button>
+                  </td>
                 </tr>
               ))}
+              {/* Fila para mostrar el total */}
+              <tr>
+                <td colSpan="3" className="text-end fw-bold">
+                  Total:
+                </td>
+                <td>
+                  $
+                  {selectedProducts.reduce(
+                    (total, product) =>
+                      total + product.price * product.quantity,
+                    0
+                  )}
+                </td>
+              </tr>
             </tbody>
           </Table>
         </Card.Body>
@@ -88,5 +151,27 @@ function CardSaleApp() {
     </div>
   );
 }
+
+const initialData = [
+  { name: "Tortillas Gruesas", price: 5 },
+  { name: "Tortillas Finas", price: 4 },
+  { name: "Pan", price: 3 },
+  { name: "Pan Lactal", price: 2 },
+  { name: "Azúcar", price: 1 },
+  { name: "Leche", price: 2 },
+  { name: "Huevos", price: 3 },
+  { name: "Mantequilla", price: 4 },
+  { name: "Harina", price: 5 },
+  { name: "Levadura", price: 6 },
+  { name: "Miel", price: 7 },
+  { name: "Galletas", price: 8 },
+  { name: "Croissants", price: 9 },
+  { name: "Pastelitos", price: 10 },
+  { name: "Donas", price: 11 },
+  { name: "Bagels", price: 12 },
+  { name: "Panes Integrales", price: 13 },
+  { name: "Bollo", price: 14 },
+  { name: "Panecillos", price: 15 },
+];
 
 export default CardSaleApp;
