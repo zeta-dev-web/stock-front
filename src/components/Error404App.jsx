a3901b1279b31093db6cb27bd506dbf261736d38
Leonel Sanagua <leonel.sanagua@gmail.com>
2023-11-16 02:18:23 -0300

    Se hizo pagina de error 404 con su componentes

---
+++ src/components/Error404App.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import error404 from "../assets/error404.json";
+const Error404App = () => {
+  const playerRef = useRef(null);
+  return (
+    <div className="100-vw">
+      <Player
+        ref={playerRef}
+        autoplay={true}
+        loop={true}
+        controls={false}
+        src={error404}
+        style={{ height: "80vh", width: "80vw" }}
+      ></Player>
+    </div>
+  );
+};
+
+export default Error404App;
