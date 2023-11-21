147418b056a75a85fcdeed046101e096b72b12d3
Leonel Sanagua <leonel.sanagua@gmail.com>
2023-11-21 16:44:39 -0300

    Se hizo mejoras en la pagina de error

--- src/components/Error404App.jsx
+++ src/components/Error404App.jsx
@@ -1,20 +1,32 @@
-import React, { useRef } from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import error404 from "../assets/error404.json";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Error404.css";
+
 const Error404App = () => {
-  const playerRef = useRef(null);
   return (
-    <div className="100-vw">
-      <Player
-        className="mt-5"
-        ref={playerRef}
-        autoplay={true}
-        loop={true}
-        controls={false}
-        src={error404}
-        style={{ height: "80vh", width: "80vw" }}
-      ></Player>
-    </div>
+    <section class="page_404 d-flex justify-content-center p-0 m-0">
+      <div className="w-100">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 d-flex justify-content-center">
+              <div className="col-sm-10 col-sm-offset-1 text-center">
+                <div className="four_zero_four_bg">
+                  <h1 className="text-center">404</h1>
+                </div>
+
+                <div className="contant_box_404">
+                  <h3 className="h2">Parece que te perdiste</h3>
+                  <p>la página a la que quieres acceder no esta disponible</p>
+                  <Link to="/" className="link_404">
+                    Regresar al Inicio
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
