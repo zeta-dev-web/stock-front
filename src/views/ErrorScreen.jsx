9ed1d4adf93a2394e096190850291e59cae52d4a
Leonardo Zamorano <nvreparaciones@gmail.com>
2023-12-16 13:09:37 -0300

    Se corrige la superposición del Navbar con la pagina de error 404

--- src/views/ErrorScreen.jsx
+++ src/views/ErrorScreen.jsx
@@ -3,7 +3,7 @@ import Error404App from "../components/Error404App";
 
 const ErrorScreen = ({}) => {
   return (
-    <div>
+    <div className="h-100 pt-5 mt-2">
       <Error404App></Error404App>
     </div>
   );
